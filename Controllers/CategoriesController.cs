7a4ea506a56cea835f0adfb581d47fa09d6f38b1
DONGYUN HUANG <30042104@tafe.wa.edu.au>
2023-10-18 11:54:43 +0800

    home page show details as a table.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using FlowerSales.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerSalesApi.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ShopContext _shopContext;
+
+        public CategoriesController(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+            _shopContext.Database.EnsureCreated();
+        }
+
+        [HttpGet, Route("/ReadAllCategories")]
+        public ActionResult GetCategories()  // 1.c: Returning multiple items
+        {
+            return Ok(_shopContext.Categories.ToList());
+        }
+    }
+}
